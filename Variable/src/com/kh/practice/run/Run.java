a6915b3da316f40ddabf836f77aba4afed3593d6
HEEYEON <HEEYEON@DESKTOP-I0D5H95>
2022-08-06 11:11:41 +0900

    Practice Variable

---
+++ Variable/src/com/kh/practice/run/Run.java
@@ -0,0 +1,20 @@
+package com.kh.practice.run;
+
+import com.kh.practice.example.VariablePractice;
+
+public class Run {
+
+	public static void main(String[] args) {
+
+		VariablePractice v = new VariablePractice();
+
+		v.method1();
+
+		v.method2();
+
+		v.method3();
+
+		v.method4();
+	}
+
+} 
